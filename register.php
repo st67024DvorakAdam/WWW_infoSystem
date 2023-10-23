88195ecc7142c089397fdaff9f2d106403da6b13
st67289 <davidecekcz@gmail.com>
2023-10-23 19:03:18 +0200

    Update

--- register.php
+++ register.php
@@ -22,7 +22,7 @@ $url = "index.php";
 </head>
 
 <body>
-    
+
     <section class="p-3 p-md-4 p-xl-5" style="margin-top: 120px;">
         <div class="container">
             <div class="card border-light-subtle shadow-sm">
@@ -30,8 +30,9 @@ $url = "index.php";
                     <div class="col-12 col-md-6 text-bg-primary">
                         <div class="d-flex align-items-center justify-content-center h-100">
                             <div class="col-10 col-xl-8 py-3">
-                                <img class="img-fluid rounded mb-4" loading="lazy" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c0840e59-db43-4681-ae7b-31a04dc4bc55/d7eqdvw-4e97ac92-e4b9-4498-9655-e4d612eb478b.png/v1/fill/w_1192,h_670/random_logo_by_criticl_d7eqdvw-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvYzA4NDBlNTktZGI0My00NjgxLWFlN2ItMzFhMDRkYzRiYzU1XC9kN2VxZHZ3LTRlOTdhYzkyLWU0YjktNDQ5OC05NjU1LWU0ZDYxMmViNDc4Yi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.X991O1jF5lTNZbbEoHEfoo6nlHEihBMHMIm5-uBCXcU" width="400" height="160"
-                                    alt="">
+                                <img class="img-fluid rounded mb-4" loading="lazy"
+                                    src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c0840e59-db43-4681-ae7b-31a04dc4bc55/d7eqdvw-4e97ac92-e4b9-4498-9655-e4d612eb478b.png/v1/fill/w_1192,h_670/random_logo_by_criticl_d7eqdvw-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvYzA4NDBlNTktZGI0My00NjgxLWFlN2ItMzFhMDRkYzRiYzU1XC9kN2VxZHZ3LTRlOTdhYzkyLWU0YjktNDQ5OC05NjU1LWU0ZDYxMmViNDc4Yi5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.X991O1jF5lTNZbbEoHEfoo6nlHEihBMHMIm5-uBCXcU"
+                                    width="400" height="160" alt="">
                                 <hr class="border-primary-subtle mb-4">
                                 <h2 class="h1 mb-4">INFOSYSTEM</h2>
                                 <p class="lead m-0">Nějakej text</p>
@@ -55,81 +56,88 @@ $url = "index.php";
                                         <input type="email" class="form-control" name="email" id="email"
                                             placeholder="Jan" required>
                                     </div>
-                                    </div>
-                                    
-                            <form action="login.php" method="post">
-                                <div class="row gy-3 gy-md-4 overflow-hidden">
-                                    <div class="col-12">
-                                        <label for="surname" class="form-label">Surname <span
-                                                class="text-danger">*</span></label>
-                                        <input type="email" class="form-control" name="email" id="email"
-                                            placeholder="Novak" required>
-                                    </div>
-                                    <div class="col-12">
-                    <label for="gender" class="form-label">Gender</label>
-                    <select class="form-select" name="gender" id="gender" required>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
-                </div>
-                            <form action="login.php" method="post">
-                                <div class="row gy-3 gy-md-4 overflow-hidden">
-                                    <div class="col-12">
-                                        <label for="email" class="form-label">Email <span
-                                                class="text-danger">*</span></label>
-                                        <input type="email" class="form-control" name="email" id="email"
-                                            placeholder="vasemail@priklad.com" required>
-                                    </div>
-                                    <div class="col-12">
-                    <label for="phone" class="form-label">Phone Number</label>
-                    <input type="tel" class="form-control" name="phone" id="phone" placeholder="123-456-7890">
-                </div>
-                <div class="col-12">
-                    <label for="profile-picture" class="form-label">Profile Picture</label>
-                    <input type="file" class="form-control" name="profile-picture" id="profile-picture">
-                </div>
-                                    <div class="col-12">
-                                        <label for="password" class="form-label">Password <span
-                                                class="text-danger">*</span></label>
-                                        <input type="password" class="form-control" name="password" id="password"
-                                            value="" required>
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="password" class="form-label">Password again <span
-                                                class="text-danger">*</span></label>
-                                        <input type="password" class="form-control" name="password" id="password"
-                                            value="" required>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" name="remember_me"
-                                                id="remember_me">
-                                            <label class="form-check-label text-secondary" for="remember_me">
-                                                Zapamatovat si mě
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="d-grid">
-                                            <button class="btn bsb-btn-xl btn-primary" type="submit">Přihlásit se</button>
-                                        </div>
-                                    </div>
                                 </div>
-                            </form>
-                            <div class="row">
-                                <div class="col-12">
-                                    <hr class="mt-5 mb-4 border-secondary-subtle">
-                                    <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end">
-                                        <a href="index.php" class="link-secondary text-decoration-none">Již máte vytvořený účet</a>
-                                    </div>
-                                </div>
-                            </div>
 
+                                <form action="login.php" method="post">
+                                    <div class="row gy-3 gy-md-4 overflow-hidden">
+                                        <div class="col-12">
+                                            <label for="surname" class="form-label">Surname <span
+                                                    class="text-danger">*</span></label>
+                                            <input type="email" class="form-control" name="email" id="email"
+                                                placeholder="Novak" required>
+                                        </div>
+                                        <div class="col-12">
+                                            <label for="gender" class="form-label">Gender</label>
+                                            <select class="form-select" name="gender" id="gender" required>
+                                                <option value="male">Male</option>
+                                                <option value="female">Female</option>
+                                            </select>
+                                        </div>
+                                        <form action="login.php" method="post">
+                                            <div class="row gy-3 gy-md-4 overflow-hidden">
+                                                <div class="col-12">
+                                                    <label for="email" class="form-label">Email <span
+                                                            class="text-danger">*</span></label>
+                                                    <input type="email" class="form-control" name="email" id="email"
+                                                        placeholder="vasemail@priklad.com" required>
+                                                </div>
+                                                <div class="col-12">
+                                                    <label for="phone" class="form-label">Phone Number</label>
+                                                    <input type="tel" class="form-control" name="phone" id="phone"
+                                                        placeholder="123-456-7890">
+                                                </div>
+                                                <div class="col-12">
+                                                    <label for="profile-picture" class="form-label">Profile
+                                                        Picture</label>
+                                                    <input type="file" class="form-control" name="profile-picture"
+                                                        id="profile-picture">
+                                                </div>
+                                                <div class="col-12">
+                                                    <label for="password" class="form-label">Password <span
+                                                            class="text-danger">*</span></label>
+                                                    <input type="password" class="form-control" name="password"
+                                                        id="password" value="" required>
+                                                </div>
+                                                <div class="col-12">
+                                                    <label for="password" class="form-label">Password again <span
+                                                            class="text-danger">*</span></label>
+                                                    <input type="password" class="form-control" name="password"
+                                                        id="password" value="" required>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="checkbox" value=""
+                                                            name="remember_me" id="remember_me">
+                                                        <label class="form-check-label text-secondary"
+                                                            for="remember_me">
+                                                            Zapamatovat si mě
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-12">
+                                                    <div class="d-grid">
+                                                        <button class="btn bsb-btn-xl btn-primary"
+                                                            type="submit">Přihlásit se</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <hr class="mt-5 mb-4 border-secondary-subtle">
+                                                <div
+                                                    class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end">
+                                                    <a href="index.php" class="link-secondary text-decoration-none">Již
+                                                        máte vytvořený účet</a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     </section>
 </body>
 
