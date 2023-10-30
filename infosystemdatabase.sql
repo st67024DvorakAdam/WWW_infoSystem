0e3eb7b58aaa68fe95fdbc7b1bcd3097052ebcaa
st67024DvorakAdam <125569481+st67024DvorakAdam@users.noreply.github.com>
2023-10-30 16:53:14 +0100

    img_path predelano na img (blob)

--- infosystemdatabase.sql
+++ infosystemdatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 30. říj 2023, 14:19
+-- Vytvořeno: Pon 30. říj 2023, 16:51
 -- Verze serveru: 10.4.28-MariaDB
 -- Verze PHP: 8.2.4
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabulky `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `sender_id` int(10) UNSIGNED NOT NULL,
+  `receiver_id` int(10) UNSIGNED NOT NULL,
+  `text` varchar(1024) NOT NULL,
+  `messageDateTime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabulky `post`
 --
 
@@ -35,9 +49,43 @@ CREATE TABLE `post` (
   `user_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `sex` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `password` varchar(1024) NOT NULL,
+  `is_administrator` tinyint(1) NOT NULL DEFAULT 0,
+  `img` blob DEFAULT NULL,
+  `register_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Vypisuji data pro tabulku `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `sex`, `email`, `phone_number`, `password`, `is_administrator`, `img`, `register_date`) VALUES
+(11, 'David', 'Kokot', 'male', 'davidecekcz@gmail.com', NULL, '$2y$10$qWGLeW63eldKZ8TS1n9aveIE7NlthSwgkvQ793JfcIHuZP84lSIha', 0, NULL, '2023-10-23 20:40:03');
+
 --
 -- Indexy pro exportované tabulky
 --
+
+--
+-- Indexy pro tabulku `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `receiver_id` (`receiver_id`);
 
 --
 -- Indexy pro tabulku `post`
@@ -47,8 +95,20 @@ ALTER TABLE `post`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexy pro tabulku `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pro tabulky
 --
+
+--
+-- AUTO_INCREMENT pro tabulku `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pro tabulku `post`
@@ -57,8 +117,21 @@ ALTER TABLE `post`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pro tabulku `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- Omezení pro exportované tabulky
 --
+
+--
+-- Omezení pro tabulku `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Omezení pro tabulku `post`
